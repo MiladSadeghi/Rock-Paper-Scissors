7305188622cbb12a0366a6841c15ce2f13e6ba30
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-12 20:13:27 +0430

    The photo changes according to the selection

--- JS/main.js
+++ JS/main.js
@@ -2,19 +2,73 @@ const rock = document.getElementById('rock')
 const paper = document.getElementById('paper')
 const scissors = document.getElementById('scissors')
 
-
+let winCount = 0
+let drawCount = 0
+let loseCount = 0
 
 eventListeners()
 function eventListeners() {
-  rock.addEventListener('click', () => game('Rock'))
-  paper.addEventListener('click', () => game('Paper'))
-  scissors.addEventListener('click', () => game('Scissors'))
+  rock.addEventListener('click', () => imageRotation('Rock'))
+  paper.addEventListener('click', () => imageRotation('Paper'))
+  scissors.addEventListener('click', () => imageRotation('Scissors'))
 }
 
 function computerChoice() {
   const choices = ['Rock', 'Paper', 'Scissors']
   const random = Math.floor(Math.random() * 3)
   return choices[random]
+}
+
+function imageRotation(userChoice) {
+  const imageUs = document.querySelector('.we-choice img')
+  const imageComp = document.querySelector('.bot-choice img')
+  imageUs.src = "../Assest/Images/Rock.png"
+  imageComp.src = "../Assest/Images/comp-Rock.png"
+
+  const getFromComputer = computerChoice()
+
+  imageUs.animate([
+    {transform: 'rotate(0)'},
+    {transform: 'rotate(-40deg)'}
+  ], {
+    duration: 400,
+    delay: 50,
+    iterations: 6,
+    direction: 'alternate'
+  })
+
+  imageComp.animate([
+    {transform: 'rotate(0)'},
+    {transform: 'rotate(40deg)'}
+  ], {
+    duration: 400,
+    delay: 50,
+    iterations: 6,
+    direction: 'alternate'
+  })
+
+  setTimeout(() => {
+    switch (userChoice) {
+      case 'Paper':
+        imageUs.src = '../Assest/Images/Paper.png'
+        break;
+      case 'Scissors':
+        imageUs.src = '../Assest/Images/Scissors.png'
+        break;
+    }
+    switch (getFromComputer) {
+      case 'Rock':
+        imageComp.src = '../Assest/Images/comp-Rock.png'
+        break;
+      case 'Paper':
+        imageComp.src = '../Assest/Images/comp-Paper.png'
+        break;
+      case 'Scissors':
+        imageComp.src = '../Assest/Images/comp-Scissors.png'
+        break;
+    }
+    
+  }, 2400);
 }
 
 function game(userChoice) {
@@ -40,4 +94,3 @@ function game(userChoice) {
       break;
   }
 }
-
