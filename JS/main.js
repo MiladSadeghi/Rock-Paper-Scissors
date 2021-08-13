31c523c8f1c3f645bc6c96946ac00a4e2e98f1e4
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-13 22:26:04 +0430

    add result info to rounds section

--- JS/main.js
+++ JS/main.js
@@ -1,141 +1,187 @@
-const rock = document.getElementById('rock')
-const paper = document.getElementById('paper')
-const scissors = document.getElementById('scissors')
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
 
-let winCount = 0
-let drawCount = 0
-let loseCount = 0
-let round = 1
+let winCount = 0;
+let drawCount = 0;
+let loseCount = 0;
+let round = 1;
 
-eventListeners()
+eventListeners();
 function eventListeners() {
-  rock.addEventListener('click', () => imageRotation('Rock'))
-  paper.addEventListener('click', () => imageRotation('Paper'))
-  scissors.addEventListener('click', () => imageRotation('Scissors'))
+  rock.addEventListener("click", () => imageRotation("Rock"));
+  paper.addEventListener("click", () => imageRotation("Paper"));
+  scissors.addEventListener("click", () => imageRotation("Scissors"));
 }
 
 function computerChoice() {
-  const choices = ['Rock', 'Paper', 'Scissors']
-  const random = Math.floor(Math.random() * 3)
-  return choices[random]
+  const choices = ["Rock", "Paper", "Scissors"];
+  const random = Math.floor(Math.random() * 3);
+  return choices[random];
 }
 
 function imageRotation(userChoice) {
-  const imageUs = document.querySelector('.we-choice img')
-  const imageComp = document.querySelector('.bot-choice img')
-  imageUs.src = "../Assest/Images/Rock.png"
-  imageComp.src = "../Assest/Images/comp-Rock.png"
+  const imageUs = document.querySelector(".we-choice img");
+  const imageComp = document.querySelector(".bot-choice img");
+  imageUs.src = "../Assest/Images/Rock.png";
+  imageComp.src = "../Assest/Images/comp-Rock.png";
 
-  const getFromComputer = computerChoice()
+  const getFromComputer = computerChoice();
 
-  imageUs.animate([
-    {transform: 'rotate(0)'},
-    {transform: 'rotate(-40deg)'}
-  ], {
-    duration: 400,
-    delay: 50,
-    iterations: 6,
-    direction: 'alternate'
-  })
+  imageUs.animate(
+    [{ transform: "rotate(0)" }, { transform: "rotate(-40deg)" }],
+    {
+      duration: 400,
+      delay: 50,
+      iterations: 6,
+      direction: "alternate",
+    }
+  );
 
-  imageComp.animate([
-    {transform: 'rotate(0)'},
-    {transform: 'rotate(40deg)'}
-  ], {
-    duration: 400,
-    delay: 50,
-    iterations: 6,
-    direction: 'alternate'
-  })
+  imageComp.animate(
+    [{ transform: "rotate(0)" }, { transform: "rotate(40deg)" }],
+    {
+      duration: 400,
+      delay: 50,
+      iterations: 6,
+      direction: "alternate",
+    }
+  );
 
   setTimeout(() => {
     switch (userChoice) {
-      case 'Paper':
-        imageUs.src = '../Assest/Images/Paper.png'
+      case "Paper":
+        imageUs.src = "../Assest/Images/Paper.png";
         break;
-      case 'Scissors':
-        imageUs.src = '../Assest/Images/Scissors.png'
+      case "Scissors":
+        imageUs.src = "../Assest/Images/Scissors.png";
         break;
     }
     switch (getFromComputer) {
-      case 'Rock':
-        imageComp.src = '../Assest/Images/comp-Rock.png'
+      case "Paper":
+        imageComp.src = "../Assest/Images/comp-Paper.png";
         break;
-      case 'Paper':
-        imageComp.src = '../Assest/Images/comp-Paper.png'
-        break;
-      case 'Scissors':
-        imageComp.src = '../Assest/Images/comp-Scissors.png'
+      case "Scissors":
+        imageComp.src = "../Assest/Images/comp-Scissors.png";
         break;
     }
-    game(userChoice ,getFromComputer)
+    game(userChoice, getFromComputer);
+    rounds(userChoice, getFromComputer);
   }, 2400);
 }
 
 function game(userChoice, getFromComputer) {
-  const userComp = userChoice+getFromComputer
-  
+  const userComp = userChoice + getFromComputer;
+
   switch (userComp) {
-    case 'RockScissors':
-    case 'PaperRock':
-    case 'ScissorsPaper':
-      win()
+    case "RockScissors":
+    case "PaperRock":
+    case "ScissorsPaper":
+      document.getElementsByClassName('.first').style.Color = '#c8002b'
+      win();
       break;
-    
-    case 'RockPaper':
-    case 'PaperScissors':
-    case 'ScissorsRock':
-      lose()
+
+    case "RockPaper":
+    case "PaperScissors":
+    case "ScissorsRock":
+      document.querySelector('.second').style.color = '#c8002b'
+      lose();
       break;
-    
-    case 'RockRock':
-    case 'PaperPaper':
-    case 'ScissorsScissors':
-      draw()
+
+    case "RockRock":
+    case "PaperPaper":
+    case "ScissorsScissors":
+      draw();
       break;
   }
 }
 
 function win() {
-  winCount++
-  round++
-  document.getElementById('win').innerHTML = winCount
-  document.getElementById('round-counter').innerHTML = round
-  result('You Win!')
+  winCount++;
+  round++;
+  document.getElementById("win").innerHTML = winCount;
+  document.getElementById("round-counter").innerHTML = round;
+  result("You Win!");
 }
 
 function lose() {
-  loseCount++
-  round++
-  document.getElementById('lose').innerHTML = loseCount
-  document.getElementById('round-counter').innerHTML = round
-  result('You Lose!')
+  loseCount++;
+  round++;
+  document.getElementById("lose").innerHTML = loseCount;
+  document.getElementById("round-counter").innerHTML = round;
+  result("You Lose!");
 }
 
 function draw() {
-  drawCount++
-  round++
-  document.getElementById('draw').innerHTML = drawCount
-  document.getElementById('round-counter').innerHTML = round
-  result('Draw!')
+  drawCount++;
+  round++;
+  document.getElementById("draw").innerHTML = drawCount;
+  document.getElementById("round-counter").innerHTML = round;
+  result("Draw!");
 }
 
 function result(res) {
-  const resDivs = document.querySelector('.winner')
-  if(resDivs.children.length >= 3) {
-    resDivs.children[0].remove()
+  const resDivs = document.querySelector(".winner");
+  if (resDivs.children.length >= 3) {
+    resDivs.children[0].remove();
   }
 
-  const resSection = document.querySelector('.winner')
-  const div = document.createElement('div')
-  div.classList.add('winner-content')
+  const resSection = document.querySelector(".winner");
+  const div = document.createElement("div");
+  div.classList.add("winner-content");
   div.innerHTML += `
       <hr>
       <div class="result">
         <p>${round - 1}</p>
         <span>${res}</span>
       </div>
-  `
+  `;
+  resSection.appendChild(div);
+}
 
-  resSection.appendChild(div)
+function rounds(we, they) {
+  const roundSection = document.querySelector(".counter-rounds");
+  const div = document.createElement("div");
+  div.classList.add('mainIcons')
+  let paperIcon
+  let paperCompIcon
+
+  switch (we) {
+    case "Paper":
+      paperIcon = `<i class="roundPIcon far fa-hand-paper"></i>`;
+      break;
+
+    case "Rock":
+      paperIcon = `<i class="roundPIcon far fa-hand-rock"></i>`;
+      break;
+
+    case "Scissors":
+      paperIcon = `<i class="roundGoodIcon far fa-hand-scissors"></i>`;
+      break;
+  }
+
+  switch (they) {
+    case "Paper":
+      paperCompIcon = `<i class="roundNIcon far fa-hand-paper"></i>`;
+      break;
+
+    case "Rock":
+      paperCompIcon = `<i class="roundNIcon far fa-hand-rock"></i>`;
+      break;
+
+    case "Scissors":
+      paperCompIcon = `<i class="far fa-hand-scissors"></i>`;
+      break;
+  }
+
+  div.innerHTML += `
+    <hr>
+    <div class="rounds-content">
+      <p class="first">${paperIcon}</p>
+      <span>${round - 1}</span>
+      <p class="second">${paperCompIcon}</p>
+    </div>
+  `;
+
+  roundSection.appendChild(div);
 }
